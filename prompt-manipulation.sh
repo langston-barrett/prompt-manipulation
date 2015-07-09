1e5257efd455b9459619dbe6d6ac03d77fed5bfc
siddharthist <langston.barrett@gmail.com>
2015-07-09 15:40:55 -0500

    First code commit

--- prompt-manipulation.sh
+++ prompt-manipulation.sh
@@ -1,71 +1,49 @@
-# prompt-manipulation
-Manipulate bash/zsh prompts in your shell scripts!
+#!/bin/bash
 
-### Installation
-```bash
-wget TODO
-```
+# store in the environment, in case this script is quit without resetting
+export PROMPT_MANIPULATION_OLD=$PS1
+# store locally in case environment is manipulated during run
+old=$PS1
 
-### Usage
-```bash
-source prompt_manipulation.sh
-```
-or
-```bash
-. prompt_manipulation.sh
-```
-and then just call one of the functions!
-
-### Provided functions
-```bash
 # Description: Echos the prompt prefix as a string
 # Arguments: none
 function current_prompt() {
-    ...
+    echo $PS1
 }
 
 # Description: Prefixes the user's shell prompt with a given string.
 # Arguments: string to put in front of the prompt.
 function prefix_prompt() {
-    ...
+    PS1=$1$PS1
 }
 
 # Description: Suffixes the user's shell prompt with a given string.
 # Arguments: string to put behind the prompt.
 function suffix_prompt() {
-    ...
+    PS1=$PS1$1
 }
 
 # Description: Replaces the user's shell prompt with a given string.
 # Arguments: string to replace the prompt.
 function replace_prompt() {
-    ...
+    PS1=$1
 }
 
 # Description: Reset the prompt to whatever it was before your script ran.
 # Should be called every time you use one of the above functions.
 # Arguments: none
 function reset_prompt() {
-    ...
+    # -z tests if this variable is zero length (i.e. unset)
+    # -n tests if this variable is not zero length (i.e. set)
+    if [ -z "$PROMPT_MANIPULATION_OLD" ] && [ -z "$old" ]; then
+        echo "[ERROR] There was no old prompt to reset to!"
+    elif [ -n "$old" ]; then
+        PS1=$old
+        export PS1
+    elif [ -n "$PROMPT_MANIPULATION_OLD" ]; then
+        PS1=$PROMPT_MANIPULATION_OLD
+        export PS1
+    fi
+    unset PROMPT_MANIPULATION_OLD
+    unset old
 }
-```
-
-### Examples
-
-Prompt: `$ `
-Code: `prompt_prefix "prompt"`
-Prompt: `prompt$ `
-Code: `reset_prompt`
-Prompt: `$ `
-
-Prompt: `user@hostname `
-Code: `prompt_suffix "DB>"`
-Prompt: `user@hostname DB>`
-Code: `reset_prompt`
-Prompt: `user@hostname `
-
-Prompt: `user [11:09:56]>`
-Code: `prompt_replace ">>>"`
-Prompt: `>>>`
-Code: `reset_prompt`
-Prompt: `user [11:09:56]>`
