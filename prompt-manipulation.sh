3e8a4173a2c28d5353bf9770e282d454ea225eca
siddharthist <langston.barrett@gmail.com>
2015-08-09 16:39:36 -0500

    Reformatted

--- prompt-manipulation.sh
+++ prompt-manipulation.sh
@@ -2,9 +2,7 @@
 
 # Description: Echos the prompt prefix as a string
 # Arguments: none
-function current_prompt() {
-    echo "$PS1"
-}
+function current_prompt() { echo "$PS1" ; }
 
 # Description: store the provided prompt as a backup in as many places as we can!
 # Arguments: prompt to store
@@ -30,27 +28,19 @@ function unstore_prompt() {
 # Description: store the current prompt as a backup in as many places as we can!
 # Arguments: none
 # Returns (echoes): none
-function store_current_prompt() {
-    store_prompt "$(current_prompt)"
-}
+function store_current_prompt() { store_prompt "$(current_prompt)" ; }
 
 # Description: Prefixes the user's shell prompt with a given string.
 # Arguments: string to put in front of the prompt.
-function prefix_prompt() {
-    PS1=$1$PS1
-}
+function prefix_prompt() { PS1=$1$PS1 ; }
 
 # Description: Suffixes the user's shell prompt with a given string.
 # Arguments: string to put behind the prompt.
-function suffix_prompt() {
-    PS1=$PS1$1
-}
+function suffix_prompt() { PS1=$PS1$1 ; }
 
 # Description: Replaces the user's shell prompt with a given string.
 # Arguments: string to replace the prompt.
-function replace_prompt() {
-    PS1=$1
-}
+function replace_prompt() { PS1=$1 ; }
 
 # Description: Reset the prompt to whatever it was before your script ran.
 # The inverse of prefix_prompt, replace_prompt, etc.
@@ -58,14 +48,15 @@ function replace_prompt() {
 # Arguments: none
 # Returns (echoes): none
 function reset_prompt() {
-    if [ -z "$PROMPT_MANIPULATION_OLD" ] && [ -z "$prompt_manipulation_old" ]; then
+    if [ -z "$PROMPT_MANIPULATION_OLD" ] && [ -z "$prompt_manipulation_old" ];
+    then
         echo "[ERROR] There was no old prompt to reset to!"
-    elif [ -n "$prompt_manipulation_old" ]; then
+    fi
+    if [ -n "$prompt_manipulation_old" ]; then
         PS1=$prompt_manipulation_old
-        export PS1
     elif [ -n "$PROMPT_MANIPULATION_OLD" ]; then
         PS1=$PROMPT_MANIPULATION_OLD
-        export PS1
     fi
+    export PS1
     unstore_prompt
 }
